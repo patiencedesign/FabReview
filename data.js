3e3cb5c164d5b8cf862577df80e32e600faf2f8d
Patience Elliott <elliottpatience7@gmail.com>
2017-01-22 08:07:43 +0000

    Modals

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 var sampleReviews = [
         review1 = {
-            itemName: "Item Name",
-            requestedBy: "Requested By",
-            description: "Description",
+            itemName: "Old Navy Striped Sweater",
+            requestedBy: "Requested By FrugalFrannie",
+            description: "Long sleeved, black and white striped sweater. True to size. Color Fading.",
             media: ["media1", "media2", "media3"],
-            rating: 5
+            rating: 3
         },
         review2 = {
             itemName: "Item Name",
--- script.js
+++ script.js
@@ -1,21 +1,24 @@
+// Posts Data to Featured Section
+
 for (i = 0; i < sampleReviews.length; i++){
     $("#featured").append(
         "<div class='panel panel-default'><div class='panelbody'>" +
             sampleReviews[i].itemName + "<br>" +
-            sampleReviews[i].description + "<br>" +
+            sampleReviews[i].requestedBy + "<br>" +
+        "</div></div>"
+        
+        )
+} 
+
+// Posts Data to Latest Section
+
+for (i = 0; i < sampleReviews.length; i++){
+    $("#latest").append(
+        "<div class='panel panel-default'><div class='panelbody'>" +
+            sampleReviews[i].itemName + "<br>" +
             sampleReviews[i].requestedBy + "<br>" +
         "</div></div>"
         
         )
 }
 
-for (i = 0; i < sampleReviews.length; i++){
-    $("#latest").append(
-        "<div class='panel panel-default'><div class='panelbody'>" +
-            sampleReviews[i].itemName + "<br>" +
-            sampleReviews[i].description + "<br>" +
-            sampleReviews[i].requestedBy + "<br>" +
-        "</div></div>"
-        
-        )
-}
