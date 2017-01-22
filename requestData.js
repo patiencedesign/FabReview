82179c086bcdc8a5d817220f42ae82e3fdbfbdca
Patience Elliott <elliottpatience7@gmail.com>
2017-01-22 02:25:41 +0000

    Added Pages

---
+++ requestData.js
@@ -0,0 +1,32 @@
+var requestData = {
+    user1: {
+        name: "Patience",
+        itemLink: "SampleLink",
+        date: 170119,
+        votes: 25
+    },
+    user2: {
+        name: "Annie",
+        itemLink: "SampleLink",
+        date: 170119,
+        votes: 30
+    },
+    user3: {
+        name: "Kate",
+        itemLink: "SampleLink",
+        date: 170119,
+        votes: 35
+    },
+    user4: {
+        name: "Kim",
+        itemLink: "SampleLink",
+        date: 170119,
+        votes: 40
+    },
+    user5: {
+        name: "Julie",
+        itemLink: "SampleLink",
+        date: 170119,
+        votes: 45
+    }
+}
