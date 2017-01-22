8ac5cbbb74cd81953e3d71aab61dd622fad81453
Patience Elliott <elliottpatience7@gmail.com>
2017-01-22 18:46:03 +0000

    New Database

---
+++ database.js
@@ -0,0 +1,26 @@
+var brandDatabase = [
+        brand1 = [
+            item1 = {
+                name: "Item Name",
+                type: "Item Type",
+                review: "Review"
+            },
+            item2 = {
+                name: "Item Name",
+                type: "Item Type",
+                review: "Review"
+            }
+        ],
+        brand2 = [
+            item1 = {
+                name: "Item Name",
+                type: "Item Type",
+                review: "Review"
+            },
+            item2 = {
+                name: "Item Name",
+                type: "Item Type",
+                review: "Review"
+            }
+        ]
+    ]
